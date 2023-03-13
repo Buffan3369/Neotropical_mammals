adfdbfd0e40ec8b512849953413ca60a63dc4304
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-03-13 17:49:57 +0100

    erased time inconsistencies

--- 2-time_binning.R
+++ 2-time_binning.R
@@ -5,41 +5,29 @@ library(palaeoverse)
 library(tibble)
 library(readxl)
 
-## Generate time bins covering the whole Cenozoic + Cretaceous by Epoch -----
+## Generate time bins covering the whole Cenozoic + Cretaceous by Epoch --------
 Cnz_Mz <- palaeoverse::time_bins(interval = c("Cenozoic","Mesozoic"),
                                        rank = "epoch")
 
-data <- readxl::read_xlsx("E:/Internship_ISEM/Neotropical_Mammals/species_list.xlsx")
-age_range <- data.frame(max_ma = unlist(lapply(X = data$`Max age`, FUN = as.numeric)),
-                        min_ma = unlist(lapply(X = data$`Min age`, FUN = as.numeric)))
-binning <- bin_time(occdf = age_range,
-                    bins = Cnz_Mz,
-                    method = "majority")
-write.csv(binning, "E:/Internship_ISEM/Neotropical_Mammals/binning.csv")
+## Load data and extract age range of each occurrence --------------------------
+data_2023 <- read.csv("./data_2023/Neotropical_Mammals_COMBINED.csv")
 
-
-
-
+## Check potential time inconsistencies (i.e. typos where max_ma < min_ma) -----
 time_insconsistency <- function(x){
-  if(data$`Min age` > data$`Max age`){
+  if(data_2023$Min.age[x] > data_2023$Max.age[x]){
     return(x)
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
+incons <- unlist(lapply(X = 1:nrow(data_2023), FUN = time_insconsistency))
+for(pos in incons){
+  max <- data_2023$Max.age[pos]
+  min <- data_2023$Min.age[pos]
+  #invert max and min
+  data_2023$Min.age[pos] <- max
+  data_2023$Max.age[pos] <- min
+}
 
 ## Temporal binning of our occurrences -----------------------------------------
-data_2023 <- read.csv("./data_2023/Neotropical_Mammals_COMBINED.csv")
 binning <- bin_time(occdf = data.frame(max_ma = data_2023$Max.age,
                                        min_ma = data_2023$Min.age),
                     bins = Cnz_Mz,
