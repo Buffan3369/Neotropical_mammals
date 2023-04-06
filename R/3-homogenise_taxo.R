6acc634644e8ac8dd96d327ed6c8b3f9c7ba6c62
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-04-06 17:21:17 +0200

    save order-level PyRate inputs

--- R/3-homogenise_taxo.R
+++ R/3-homogenise_taxo.R
@@ -37,10 +37,25 @@ write.csv(clean_taxo, "./data_2023/homogenised_taxo.csv", row.names = FALSE, na 
 
 
 ## Split tables from the different orders --------------------------------------
-setwd("E:/Internship_ISEM/Neotropical_Mammals/DATA")
+setwd("E:/Internship_ISEM/Neotropical_Mammals/DATA/")
 species_list <- read_xlsx("species_list_without_duplicates.xlsx")
+#function returning genus-level status
+gen_level_status <- function(genus, df=species_list){
+  rpz_status <- df[which(df[, "Genus"] == genus), c("Status")] #vector of the status of the genus representatives in our species list (where we stored)
+  if("extant" %in% rpz_status){
+    return("extant")
+  }
+  else if( ("extant" %in% rpz_status == FALSE) & ("extinct" %in% rpz_status) ){ #rpz_status = c("extinct", ....) or c("extinct", ..., NA, ...)
+    return("extinct")
+  }
+  else{ #rpz_status = c(NA, ....) => means species-level occurrences we couldn't associate a status to, as genus extant but species unspecified, hence we don't know if extinct or extant
+    return("extant")
+  }
+}
+#implement it
 for(order in unique(species_list$Order)){
   tmp_order <- species_list[which(species_list$Order == order), ]
+  #save order-level table
   write.table(x = tmp_order,
               file = paste0("order_level/", order, ".txt"),
               sep = "\t",
@@ -48,4 +63,21 @@ for(order in unique(species_list$Order)){
               row.names = FALSE,
               quote = FALSE,
               dec = ",")
+  #write and save order-level PyRate input
+  raw_input <- data.frame(Species = tmp_order$Genus,
+                          Status = unlist(lapply(X = tmp_order$Genus,
+                                                 FUN = gen_level_status,
+                                                 df = tmp_order)),
+                          MinT = tmp_order$`Min age`,
+                          MaxT = tmp_order$`Max age`)
+  #save
+  write.table(raw_input, 
+              file = paste0("intermediate_cleaning_logs_05_04/",order,"_int_cleaning_05_04.txt"),
+              row.names = FALSE,
+              quote = FALSE,
+              sep = "\t",
+              na = "")
+  
 }
+
+## Order-level PyRate inputs ---------------------------------------------------
