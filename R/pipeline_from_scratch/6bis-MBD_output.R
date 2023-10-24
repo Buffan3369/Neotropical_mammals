f7a500dda8bfa2f70b5811438d5e2939ac998c9f
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-10-24 18:38:11 +0200

    on our way for violin plot!!!

--- R/pipeline_from_scratch/6bis-MBD_output.R
+++ R/pipeline_from_scratch/6bis-MBD_output.R
@@ -2,11 +2,22 @@
 ############################# Treat MBD outputs ################################
 ################################################################################
 
-library(EvoPhylo)
 library(dplyr)
+library(hash)
+library(ggplot2)
+library(rempsyc)
+
+## Dictionary containing the corresponding index of the covariates -------------
+cov_idx <- hash("0" = "Self-diversity",
+                "1" = "Plant_diversity",
+                "2" = "Andes_elevation",
+                "3" = "Temperature",
+                "4" = "Atmospheric_carbon",
+                "5" = "Organic_carbon",
+                "6" = "Sea_level")
 
 ## Hand scaling ----------------------------------------------------------------
-recap_tbl <- read.table("../../MBD/EOCENE_OLIGOCENE/hand.scale_only/Full/ESS_summary.txt", 
+recap_tbl <- read.table("../../MBD/EOCENE_OLIGOCENE/model_scaling/Full/ESS_summary.txt", 
                         sep = "\t", header = TRUE)
 if(length(which(recap_tbl$ESS_posterior < 200)) > 0){
   recap_tbl <- recap_tbl[-which(recap_tbl$ESS_posterior < 200), ]
@@ -43,7 +54,53 @@ if(length(zeros) > 0){
 if(length(mean_SW_sign) == 0){
   next
 }
-# 3) open the mcmc.log file and retain the distributions of the selected G
-mcmcLog <- combine_log("../../MBD/EOCENE_OLIGOCENE/hand.scale_only/Full/", burnin = recap_tbl$burnin[5], downsample = 1)
+# 3) open the combined mcmc.log file and retain the distributions of the selected G
+mcmcLog <- read.table("../../MBD/EOCENE_OLIGOCENE/model_scaling/Full/combined_4MBD.log",
+                      header = TRUE, sep = "\t")
+#only retain significant correlation parameters and subsample to make it lighter
+mcmcLog <- mcmcLog %>% select(any_of(corr_vbl))
+mcmcLog1 <- data.frame(apply(X = mcmcLog, FUN = sample, MARGIN = 2, size = 1000))
+colnames(mcmcLog1) <- colnames(mcmcLog)
+#reformat dataset for plotting
+equiv_param <- hash("Gl0_0"=1, "Gm0_0"=2, "Gm0_2"=3, "Gm0_3"=4, "Gm0_4"=5)
+value <- mcmcLog1[, 1]
+param <- rep(1, length(mcmcLog1[, 1]))
+if("l" %in% strsplit(colnames(mcmcLog1)[1], "")[[1]]){ #lambda
+  rate <- rep("Origination", length(mcmcLog1[, 1]))
+}
+if("m" %in% strsplit(colnames(mcmcLog1)[1], "")[[1]]){ #lambda
+  rate <- rep("Extinction", length(mcmcLog1[, 1]))
+}
+
+for(i in colnames(mcmcLog1)[-1]){
+  value <- c(value, mcmcLog1[, i])
+  param <- c(param, rep(which(colnames(mcmcLog1) == i), length(mcmcLog1[, i])))
+  if("l" %in% strsplit(i, "")[[1]]){ #lambda
+    rate <- c(rate, rep("Origination", length(mcmcLog1[, i])))
+  }
+  else if("m" %in% strsplit(i, "")[[1]]){ #mu
+    rate <- c(rate, rep("Extinction", length(mcmcLog1[, i])))
+  }
+}
+plot_df <- data.frame(value = value, param = param, rate = rate)
+#plot
+
+ggplot(data = plot_df, aes(x = factor(param), y = value)) +
+  geom_violin(adjust = .75, draw_quantiles = c(0.025, 0.975), scale = "width", aes(fill = factor(rate))) +
+  stat_summary(fun.y=mean, geom="point", size=1, color = "black") +
+  scale_color_manual(values = c("#4c4cec", "#e34a33")) +
+  scale_y_continuous(limits = c(-8, 3)) +
+  scale_x_discrete(breaks = 1:5,
+                     labels = c(keys(equiv_param))) +
+  geom_hline(yintercept = 0, linetype = "dashed", colour = "grey60") +
+  labs(x = NULL,
+       y = "Correlation coefficient",
+       fill = NULL) +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 18),
+        legend.text = element_text(size = 12),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.5)) +
+  coord_flip()
 
 
