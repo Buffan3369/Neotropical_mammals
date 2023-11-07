98ba8b3c8f7d0f356a5f615d904661e14d0069d0
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-11-07 20:10:50 +0100

    final adjustments => works now

--- constant_rate_only_BD_simulator.py
+++ constant_rate_only_BD_simulator.py
@@ -12,7 +12,6 @@ os.chdir("C:/Users/lucas/OneDrive/Bureau")
 ##########################################################################
 ###########                 SIMULATION SETTINGS                 ##########
 ##########################################################################
-
 n_reps = 1 # number of simulations
 
 # CONSTRAINTS on DATA SIZE (simulations will run until size requirements are met)
@@ -22,58 +21,61 @@ maxSP=300     # max size data set
 minEX_SP=0    # minimum number of extinct lineages allowed
 
 # SETTINGS for BD-SHIFT SIMULATIONS
-root_age = 66
-death_age = 0
+root_age = 56
+death_age = 5.33
 shift_speciation = []      # specify times of rate shifts (speciation)
 shift_extinction = []       # specify times of rate shifts (extinction)
-speciation_rates = [0.25] # if using rate shifts, the first rate is that closest to the root age
-extinction_rates = [0.15] # 
+speciation_rates = [0.3] # if using rate shifts, the first rate is that closest to the root age
+extinction_rates = [0.25] # 
 
-scale=100.
-
-
-#################### SIMULATION  FUNCTIONS #########################################
+scale=100. #determines time step of simulation => 10e-2 My here
 
 
+############################ SIMULATION  FUNCTION #############################
 def simulate(L,M,timesL,timesM,root,death,scale,s_species,maxSP,gl=0,gm=0,Dtraj=[0],Tcomp_clade=[0]):
 	ts=list()
 	te=list()
 	
 	L,M,root,death=L/scale,M/scale,int(root*scale),int(death*scale)
-    
+    #Birth-death rates
+	l=L[0]
+	m=M[0]
+    #Initialise ts
 	for i in range(s_species): 
 		ts.append(root)
-		te.append(death)
-	
-    #for time in our interval
+    #Simulate birth events
 	for t in np.arange(root, death):
-#       Constant rate birth-death 
-		l=L[0]
-		m=M[0]        
-            			
-		TE=len(te)
-		if TE>maxSP: 
+		TS=len(ts)
+        #breaking condition => we simulated enough species (threshold fixed by maxSP)
+		if TS>maxSP: 
 			break
-		for j in range(TE): # extant lineages
-			if te[j]==death:
-				ran=random.random()
-                #Birth event
-				if ran<l: 
-					te.append(death) # add species
-					ts.append(t) # sp time
-                #Death event
-				elif ran>l and ran < (l+m): # extinction
-					te[j]=t
+		for j in range(TS): # extant lineages
+			ran=random.random()
+			if ran<l:
+				ts.append(t) # sp time
+	#Now that we simulated birth events, let's move on to death events
+	for sp_t in ts:
+		t = sp_t
+		te.append(death) #initialisation of the extinction times vector
+		while t < death: #goes negatively
+			ran = random.random()
+			if ran < m:
+				te[-1] = t
+				break
+			else:
+				t += 1
+    #Convert to arrays and return unscaled            
+	ts=np.array(ts)
 	te=np.array(te)
-	return -np.array(ts)/scale, -(te)/scale
+	return -(ts)/scale, -(te)/scale
 
-############### SIMULATION SETTINGS ########################################
+############################ SIMULATION SETTINGS ##############################
 def write_to_file(f, o):
 	sumfile = open(f , "w") 
 	sumfile.writelines(o)
 	sumfile.close()
 
-
+################################# EXECUTION ###################################
 LOtrue=[0]
 n_extinct=-0
 timesL = np.sort(np.array([float(root_age),float(death_age)]+shift_speciation))[::-1]
