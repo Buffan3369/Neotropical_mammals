56cbe570e665f0c9e000e14cbe01856e72c9078a
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-11-15 14:46:17 +0100

    plotting q and ltt panels for simulations

--- R/fancy_plots/3bis_simuation_panels.R
+++ R/fancy_plots/3bis_simuation_panels.R
@@ -11,16 +11,29 @@ source("C:/Users/lucas/OneDrive/Documents/GitHub/CorsaiR/R/2-plotting_facilities
 
 ## RTT panel -------------------------------------------------------------------
 list_RTT <- list()
+list_LTT <- list()
+list_Q <- list()
 for(i in 0:9){
-  dir <- paste0("../../PyRate_outputs/simulations/combined_logs/replicate_", i, "/RTT_plots.r")
-  rtt <- extract_rtt(dir, ana = "RJMCMC")
+  #Directories
+  dir_rtt <- paste0("../../PyRate_outputs/simulations/combined_logs/replicate_", i, "/RTT_plots.r")
+  dir_ltt <- paste0("../../PyRate_outputs/simulations/LTT/replicate_", i, "/per_replicate/")
+  dir_q <- paste0("../../PyRate_outputs/simulations/Q_SHIFTS/replicate_", i, "/Parsed_Q_rates.csv")
+  #Get data
+  rtt <- extract_rtt(dir_rtt, ana = "RJMCMC")
+  ltt <- extract_ltt(dir_ltt)
+  Q <- read.table(dir_q, header = TRUE)
+  #Plotting settings
   x_lab <- NULL
-  y_lab <- NULL
+  y_lab_rtt <- NULL
+  y_lab_ltt <- NULL
+  y_lab_q <- NULL
   if(i %in% 5:9){
     x_lab <- "Time (Ma)"
   }
   if(i %in% c(0, 5)){
-    y_lab <- "Rates (events/lineage/My)"
+    y_lab_rtt <- "Rates (events/lineage/My)"
+    y_lab_ltt <- "Diversity (Nb. lineages)"
+    y_lab_q <- "Mean preservation rate (occ/lineage/My)"
   }
   #Origination and Extinction rates
   sp_ex <- rtt_plot(data = rtt,
@@ -33,16 +46,57 @@ for(i in 0:9){
                     y_limits = c(0, 1.5),
                     main = paste0("Replicate ",i),
                     x_lab = x_lab,
-                    y_lab = y_lab) +
+                    y_lab = y_lab_rtt) +
     geom_hline(yintercept = 0.17, linetype = "dashed") +
     geom_hline(yintercept = 0.1, linetype = "dashed")
   list_RTT[[i+1]] <- sp_ex
+  #Lineages through time
+  ltt_plt <- ltt_plot(ltt_df = ltt,
+                      x_breaks = c(23.03, 27.82, 33.9, 37.82, 41.2, 47.8, 56),
+                      x_labels = c(23, 27.8, 33.9, 37.8, 41.2, 47.8, 56),
+                      x_limits = c(min(max(ltt$Age), 56), 23.03),
+                      y_breaks = seq(0,(round(max(ltt$Diversity), -1) + 10),5),
+                      y_limits = c(0,(round(max(ltt$Diversity), -1) + 20)),
+                      main = paste0("Replicate ",i),
+                      x_lab = x_lab,
+                      y_lab = y_lab_ltt)
+  list_LTT[[i+1]] <- ltt_plt
+  #Preservation rates
+  Q_plot <- q_plot(data = Q, 
+                   ltt_input = ltt, 
+                   y_limits = c(0, 2.4),
+                   x_breaks = c(23.03, 27.82, 33.9, 37.82, 41.2, 47.8, 56),
+                   x_labels = c(23, 27.8, 33.9, 37.8, 41.2, 47.8, 56),
+                   x_limits = c(min(max(ltt$Age), 56), 23.03),
+                   x_lab = x_lab,
+                   y_lab = y_lab_q)
+  list_Q[[i+1]] <- Q_plot
 }
+#Do panels
 rtt_panel <- ggarrange(plotlist = list_RTT,
                        nrow = 2,
                        ncol = 5)
+ltt_panel <- ggarrange(plotlist = list_LTT,
+                       nrow = 2,
+                       ncol = 5)
+Q_panel <- ggarrange(plotlist = list_Q,
+                     nrow = 2,
+                     ncol = 5)
+#Save them
 ggsave("./figures/simulations/rtt_panel.png",
        plot = rtt_panel,
+       height = 400,
+       width = 1000,
+       units = "mm",
+       dpi = 500)
+ggsave("./figures/simulations/ltt_panel.png",
+       plot = ltt_panel,
+       height = 400,
+       width = 1000,
+       units = "mm",
+       dpi = 500)
+ggsave("./figures/simulations/Qrates_panel.png",
+       plot = Q_panel,
        height = 400,
        width = 1000,
        units = "mm",
