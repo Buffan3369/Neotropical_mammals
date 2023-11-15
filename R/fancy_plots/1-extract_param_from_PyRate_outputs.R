6e747a0a842158aaadd276b000f1aa783919f1c4
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-11-15 11:03:15 +0100

    include latest CorsaiR updates

--- R/fancy_plots/1-extract_param_from_PyRate_outputs.R
+++ R/fancy_plots/1-extract_param_from_PyRate_outputs.R
@@ -17,42 +17,83 @@ split_vector <- function(str_vec){ #str_vec has to be a stringged vector, i.e "c
   return(vec_num)
 }
 
-extract_rtt <- function(path){ #path = PATH TO THE .R SCRIPT WRITTEN BY PyRate
+extract_rtt <- function(path, ana = c("RJMCMC", "BDS")){ #path = PATH TO THE .R SCRIPT WRITTEN BY PyRate
   #Raw output of PyRate
   raw_script <- readLines(path)
-  #Extracting time
-  time_all <- raw_script[grepl("time=", raw_script)] #all mentions of the same vector in the scipt (n=4)
-  time_str <- time_all[1] #could have been any index between 1 and 4
-  time_num <- split_vector(time_str) #convert to numeric
-  time_num <- -time_num #by default, negative
-  #Extracting mean estimates for origination, extinction and net diversification rates
-  rates <- raw_script[grepl("rate=", raw_script)] #1:origination, 2:extinction, 3:net_diversification, 4:longevity
-  sp_rate <- split_vector(rates[1])
-  ex_rate <- split_vector(rates[2])
-  net_rate <- split_vector(rates[3])
-  #Extracting max HPD for Sp and Ex rates
-  maxHPD <- raw_script[grepl("maxHPD=", raw_script)]
-  sp_maxHPD <- split_vector(maxHPD[1])
-  ex_maxHPD <- split_vector(maxHPD[2])
-  net_maxHPD <- split_vector(maxHPD[3])
-  #Extracting min HPD for Sp and Ex rates
-  minHPD <- raw_script[grepl("minHPD=", raw_script)]
-  sp_minHPD <- split_vector(minHPD[1])
-  ex_minHPD <- split_vector(minHPD[2])
-  net_minHPD <- split_vector(minHPD[3])
-  #Summarising and returning as a table
-  sp_df <- data.frame(time = time_num,
-                      sp_rate = sp_rate,
-                      sp_maxHPD = sp_maxHPD,
-                      sp_minHPD = sp_minHPD)
-  ex_df <- data.frame(ex_rate = ex_rate,
-                      ex_maxHPD = ex_maxHPD,
-                      ex_minHPD = ex_minHPD)
-  net_df <- data.frame(net_rate = net_rate,
-                      net_maxHPD = net_maxHPD,
-                      net_minHPD = net_minHPD)
-  final <- cbind.data.frame(sp_df, ex_df, net_df)
-  return(final)
+  if (ana == "RJMCMC"){
+    #Extracting time
+    time_all <- raw_script[grepl("time=", raw_script)] #all mentions of the same vector in the scipt (n=4)
+    time_str <- time_all[1] #could have been any index between 1 and 4
+    time_num <- split_vector(time_str) #convert to numeric
+    time_num <- -time_num #by default, negative
+    #Extracting mean estimates for origination, extinction and net diversification rates
+    rates <- raw_script[grepl("rate=", raw_script)] #1:origination, 2:extinction, 3:net_diversification, 4:longevity
+    sp_rate <- split_vector(rates[1])
+    ex_rate <- split_vector(rates[2])
+    net_rate <- split_vector(rates[3])
+    #Extracting max HPD for Sp and Ex rates
+    maxHPD <- raw_script[grepl("maxHPD=", raw_script)]
+    sp_maxHPD <- split_vector(maxHPD[1])
+    ex_maxHPD <- split_vector(maxHPD[2])
+    net_maxHPD <- split_vector(maxHPD[3])
+    #Extracting min HPD for Sp and Ex rates
+    minHPD <- raw_script[grepl("minHPD=", raw_script)]
+    sp_minHPD <- split_vector(minHPD[1])
+    ex_minHPD <- split_vector(minHPD[2])
+    net_minHPD <- split_vector(minHPD[3])
+    #Summarising and returning as a table
+    sp_df <- data.frame(time = time_num,
+                        sp_rate = sp_rate,
+                        sp_maxHPD = sp_maxHPD,
+                        sp_minHPD = sp_minHPD)
+    ex_df <- data.frame(ex_rate = ex_rate,
+                        ex_maxHPD = ex_maxHPD,
+                        ex_minHPD = ex_minHPD)
+    net_df <- data.frame(net_rate = net_rate,
+                         net_maxHPD = net_maxHPD,
+                         net_minHPD = net_minHPD)
+    final <- cbind.data.frame(sp_df, ex_df, net_df)
+    return(final)
+  }
+  if (ana == "BDS"){
+    #Extracting mean estimates for origination, extinction and net diversification rates
+    rates <- raw_script[grepl("L_mean=", raw_script)] 
+    sp_rate <- split_vector(rates[1])
+    rates <- raw_script[grepl("M_mean=", raw_script)] 
+    ex_rate <- split_vector(rates[1])
+    rates <- raw_script[grepl("R_mean=", raw_script)] 
+    net_rate <- split_vector(rates[1])
+    #Extracting max HPD for Sp and Ex rates
+    maxHPD <- raw_script[grepl("L_hpd_M95=", raw_script)]
+    sp_maxHPD <- split_vector(maxHPD[1])
+    maxHPD <- raw_script[grepl("M_hpd_M95=", raw_script)]
+    ex_maxHPD <- split_vector(maxHPD[1])
+    maxHPD <- raw_script[grepl("R_hpd_M95=", raw_script)]
+    net_maxHPD <- split_vector(maxHPD[1])
+    #Extracting min HPD for Sp and Ex rates
+    minHPD <- raw_script[grepl("L_hpd_m95=", raw_script)]
+    sp_minHPD <- split_vector(minHPD[1])
+    minHPD <- raw_script[grepl("M_hpd_m95=", raw_script)]
+    ex_minHPD <- split_vector(minHPD[1])
+    minHPD <- raw_script[grepl("R_hpd_m95=", raw_script)]
+    net_minHPD <- split_vector(minHPD[1])
+    #Extracting time
+    time <- raw_script[grepl("age=", raw_script)]
+    time_num<-abs(eval(parse(text=time)))
+    #Summarising and returning as a table
+    sp_df <- data.frame(time = time_num,
+                        sp_rate = sp_rate,
+                        sp_maxHPD = sp_maxHPD,
+                        sp_minHPD = sp_minHPD)
+    ex_df <- data.frame(ex_rate = ex_rate,
+                        ex_maxHPD = ex_maxHPD,
+                        ex_minHPD = ex_minHPD)
+    net_df <- data.frame(net_rate = net_rate,
+                         net_maxHPD = net_maxHPD,
+                         net_minHPD = net_minHPD)
+    final <- cbind.data.frame(sp_df, ex_df, net_df)
+    return(final)
+  }
 }
 
 extract_ltt <- function(dir){ #dir = path to the DIRECTORY where LTT PER REPLICATE were stored
@@ -61,8 +102,8 @@ extract_ltt <- function(dir){ #dir = path to the DIRECTORY where LTT PER REPLICA
   #initialisation
   ltt <- read.table(files[1], header = TRUE)
   ltt$time <- unlist(lapply(X = ltt$time, FUN = round, digits = 1))
-  if(length(which(ltt$time > 66.0)) > 0){
-    ltt <- ltt[-which(ltt$time > 66.0), c("time", "diversity")]
+  if(length(which(ltt$time > 500)) > 0){
+    ltt <- ltt[-which(ltt$time > 500), c("time", "diversity")]
   }
   else{
     ltt <- ltt[, c("time", "diversity")]
@@ -72,8 +113,8 @@ extract_ltt <- function(dir){ #dir = path to the DIRECTORY where LTT PER REPLICA
   i = 2
   for(file in files[2:length(files)]){
     f <- read.table(file, header = TRUE)
-    if(length(which(f$time > 66.0)) > 0){
-      f <- f[-which(f$time > 66.0), c("time", "diversity")]
+    if(length(which(f$time > 500)) > 0){
+      f <- f[-which(f$time > 500), c("time", "diversity")]
     }
     else{
       f <- f[, c("time", "diversity")]
--- R/fancy_plots/4-simulated_LTT_AllSpecies.R
+++ R/fancy_plots/4-simulated_LTT_AllSpecies.R
@@ -8,35 +8,16 @@ library(deeptime)
 library(hash)
 library(randomcoloR)
 
+source("./R/fancy_plots/2-plotting_facilities.R")
+
 div_dict <- hash()
 
 for(j in 0:9){
   simul_lin <- read.table(paste0("./data_2023/simulated_data/All_lineages_TsTe/sim_", j, ".txt"),
                           sep = "\t",
                           header = TRUE)
-  ts_count <- simul_lin %>% count(ts)
-  te_count <- simul_lin %>% count(te)
-  
-  time_ttl <- unique(sort(c(ts_count$ts, te_count$te), decreasing = TRUE))
-  
-  cumul_occ <- c(0)
-  for(i in 1:length(time_ttl)){
-    t <- time_ttl[i]
-    N <- cumul_occ[i] #previous
-    if(t %in% ts_count$ts == FALSE){ #necessarily in te_count
-      idx <- which(te_count$te == t)
-      cumul_occ <- c(cumul_occ, (N - te_count$n[idx])) #counted negatively, min age of the occ(s)
-    }
-    else if(t %in% te_count$te == FALSE){ #necessarily in ts_count
-      idx <- which(ts_count$ts == t)
-      cumul_occ <- c(cumul_occ, (N + ts_count$n[idx])) #counted positively, max age of the occ(s)
-    }
-    else{ #t both in min and max ages
-      idx_min <- which(te_count$te == t)
-      idx_max <- which(ts_count$ts == t)
-      cumul_occ <- c(cumul_occ, (N + ts_count$n[idx_max] - te_count$n[idx_min])) #birth - death
-    }
-  }
+  time_ttl <- true_ltt(simul_lin)[[1]]
+  cumul_occ <- true_ltt(simul_lin)[[2]]
   div_dict[paste0("time_", j)] <- time_ttl
   div_dict[paste0("div_", j)] <- cumul_occ
 }
