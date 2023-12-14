8b621faced34582f7f21300bc2609d9613fc89af
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-12-14 14:52:43 +0100

    create PyRate inputs

--- data_2023/PyRate/create_fold.sh
+++ data_2023/PyRate/create_fold.sh
@@ -4,10 +4,9 @@ for s in SALMA_kept SALMA_smoothed
 do
 	for ss in genus_level species_level
 	do
-		mkdir ./$s/$ss/1-Full
-		mkdir ./$s/$ss/2-Singleton
-		mkdir ./$s/$ss/3-Spatially_scaled
-		mkdir ./$s/$ss/4-Tropical_Extratropical
-		mkdir ./$s/$ss/5-Ecomorphotype
+		rm -r ./$s/$ss/2-Singleton
+		mv ./$s/$ss/3-Spatially_scaled ./$s/$ss/2-Spatially_scaled
+		mv ./$s/$ss/4-Tropical_Extratropical ./$s/$ss/3-Tropical_Extratropical
+		mv ./$s/$ss/5-Ecomorphotype ./$s/$ss/4-Ecomorphotype
 	done
 done
