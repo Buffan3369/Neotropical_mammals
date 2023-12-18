17d978f29ef3d0bc92d1c85ba3ab1540e23eb7dd
Buffan3369 <lucas.buffan@ens-lyon.fr>
2023-12-18 11:31:30 +0100

    Create 1-Map_occurrences.R

---
+++ R/fancy_plots/1-Map_occurrences.R
@@ -0,0 +1,31 @@
+################################################################################
+# Name: 1-Map_occurrences.R
+# Author: Lucas Buffan
+# Date: 2023-12-18
+# Aim: Visualising occurrence data and Tropical/extratropical affinities.
+################################################################################
+
+library(sf)
+library(spData)
+library(ggplot2)
+library(tidyverse)
+
+data("world")
+occdf <- readRDS("./data_2023/SPECIES_LISTS/5-Fully_cleaned_EOT_SA_Mammals_SALMA_kept_Tropics_Diet.RDS")
+
+world %>% 
+  filter(continent == "South America") %>% 
+  ggplot() + 
+  geom_sf() +
+  geom_point(data = occdf, aes(x = lng, y = lat, colour = loc)) +
+  scale_color_manual(values = c("brown", "green")) +
+  theme(panel.background = element_blank())
+
+pmap_30 <- sf::read_sf("https://gws.gplates.org/reconstruct/coastlines/?&time=30&model=PALEOMAP")
+
+pmap_30 %>% 
+  ggplot() + 
+  geom_sf() +
+  geom_point(data = occdf, aes(x = p_lng, y = p_lat, colour = loc)) +
+  scale_color_manual(values = c("brown", "green")) +
+  theme(panel.background = element_blank())
