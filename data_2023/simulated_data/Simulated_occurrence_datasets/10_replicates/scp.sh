6cef5a47ff2449ebcb4e4c9d0ccd1871b4209b42
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-01-09 09:51:28 +0100

    tune simulations

--- data_2023/simulated_data/Simulated_occurrence_datasets/10_replicates/scp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {0..9}
-do
-	sshpass -p eRooch_eap4i scp -r sampled_simulated_occurrences_${i}_PyRate.py lbuffan@162.38.181.170:~/cleaning_process/simulations/replicate_$i
-done
-
