b10778881f5560f6339c0994e52063e44b2161c3
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-01-10 16:25:14 +0100

    revised and plotted occurrence map properely

--- R/fancy_plots/1-Map_occurrences.R
+++ R/fancy_plots/1-Map_occurrences.R
@@ -13,23 +13,19 @@ library(tidyverse)
 data("world")
 occdf <- readRDS("./data_2023/SPECIES_LISTS/5-Fully_cleaned_EOT_SA_Mammals_SALMA_kept_Tropics_Diet.RDS")
 
-world %>% 
+p <- world %>% 
   filter(continent == "South America") %>% 
   ggplot() + 
-  geom_sf() +
-  geom_point(data = occdf, aes(x = lng, y = lat, colour = loc)) +
-  scale_color_manual(values = c("brown", "green")) +
-  theme(panel.background = element_blank())
+  geom_sf(aes(fill = "#fff7f3", alpha = 0.2)) +
+  geom_point(data = occdf, aes(x = lng, y = lat, colour = loc), size = 2) +
+  scale_color_manual(values = c("#5ab4ac", "#8c510a")) +
+  theme(axis.line=element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        panel.background = element_blank(),
+        legend.position="none")
 
-weird <- occdf %>% 
-  filter(lat > -10 & loc == "E")
-
-#requires internet connexion
-pmap_30 <- sf::read_sf("https://gws.gplates.org/reconstruct/coastlines/?&time=30&model=PALEOMAP")
-
-pmap_30 %>% 
-  ggplot() + 
-  geom_sf() +
-  geom_point(data = occdf, aes(x = p_lng, y = p_lat, colour = loc)) +
-  scale_color_manual(values = c("brown", "green")) +
-  theme(panel.background = element_blank())
+ggsave("./figures/Fig_S2_occurrences_map.png", plot = p, height = 10, width = 7.5)
