c9759ca2b7947a1fa96265726875b64618f505f6
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-02-05 15:35:03 +0100

    setting up MBD plots

---
+++ Bash/MBD/2-MBD_runs_convergence.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for trt in 1-Full 1-Full/Eocene_only 1-Full/Oligocene_only \
+4-Tropical_Extratropical/Tropical 4-Tropical_Extratropical/Tropical/Eocene_only 4-Tropical_Extratropical/Tropical/Oligocene_only \
+4-Tropical_Extratropical/Extratropical 4-Tropical_Extratropical/Extratropical/Eocene_only 4-Tropical_Extratropical/Extratropical/Oligocene_only \
+5-Ecomorphotype/carnivore 5-Ecomorphotype/herbivore 5-Ecomorphotype/omnivore 5-Ecomorphotype/insectivore
+do
+	python ~/Documents/GitHub/CorsaiR/Python/assess_run_convergence.py -dir "$PWD"/../../results/MBD/$trt/ -ana "MBD"
+	Rscript ~/Documents/GitHub/CorsaiR/R/plot_ess.r "$PWD"/../../results/MBD/$trt/ESS_summary.txt "$PWD"/../../results/MBD/$trt/ESS_plot.png
+done
+
---
+++ Bash/MBD/README.txt
@@ -0,0 +1,1 @@
+This folder contains essential scripts to run the Multivariate Birth-Death (MBD) model and analyse its outcomes.
