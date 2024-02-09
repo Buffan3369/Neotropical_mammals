fa87a9b0ab03d4c99ade59ce7e74764c6c580a12
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-02-09 09:57:02 +0100

    increase GTS epoch font sizes, fix weird "Mississipian" apparition

--- R/fancy_plots/palaeotemp_plot_presentation.R
+++ R/fancy_plots/palaeotemp_plot_presentation.R
@@ -2,7 +2,7 @@
 # Name: palaeotemp_plot_presentation.R
 # Author: Lucas Buffan
 # Date: 2024-02-07
-# Aim: Fancy palaeotemperature  and world map plots for Lisbon presentation
+# Aim: Fancy palaeotemperature and world map plots for Lisbon presentation
 ################################################################################
 
 ## Temp ------------------------------------------------------------------------
@@ -12,6 +12,8 @@ library(deeptime)
 Temp <- read.table("./data_2023/MBD/raw_environment_correlates/palaeotemperature/merged_veizer_westerhold_Ts.txt",
                    sep = "\t",
                    header = TRUE)
+epochs <- deeptime::epochs %>% 
+  filter(max_age <= 66)
 
 p <- Temp %>% 
   filter(Age < 60 & Age > 20) %>% 
@@ -22,12 +24,12 @@ p <- Temp %>%
        y = "Global Average Temperature (°C)") +
   geom_vline(xintercept = 33.9, linetype="dashed", color = "red", linewidth = 0.8) +
   annotate(geom = "text", x = 32.5, y = 35, label = "EOT", size = 17, colour = "red") +
-  coord_geo(dat = "epochs", size = 8, color = "grey85", abbr = FALSE, center_end_labels = TRUE) +
+  coord_geo(dat = epochs, size = 14, height = unit(4, "line"), color = "grey85", abbr = FALSE, center_end_labels = TRUE) +
   theme(axis.title = element_text(size = 24, colour = "grey85"),
         axis.line = element_line(colour = "grey85"),
         axis.ticks = element_line(colour = "grey85"),
         axis.text = element_text(size = 24, colour = "grey85"),
-        panel.background = element_rect(fill='transparent'), #transparent panel bg
+        panel.background = element_rect(fill='transparent'), #transparent   panel bg
         plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
         panel.grid.major = element_blank(), #remove major gridlines
         panel.grid.minor = element_blank(), #remove minor gridlines
