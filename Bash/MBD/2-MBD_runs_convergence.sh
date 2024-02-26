0db067e5e4dd7c4d20206f527eda1643ce848bea
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-02-26 17:54:16 +0100

    process new MBDs Tropics/Extratropics

--- Bash/MBD/2-MBD_runs_convergence.sh
+++ Bash/MBD/2-MBD_runs_convergence.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-for trt in  5-Ecomorphotype/herbivore  5-Ecomorphotype/herbivore/post_EECO  5-Ecomorphotype/herbivore/Oligocene
+for trt in 4-Tropical_Extratropical/Tropical 4-Tropical_Extratropical/Tropical/Eocene_only 4-Tropical_Extratropical/Tropical/Oligocene_only \
+	4-Tropical_Extratropical/Extratropical 4-Tropical_Extratropical/Extratropical/Eocene_only 4-Tropical_Extratropical/Extratropical/Oligocene_only \
 #1-Full/early_phase 1-Full/late_phase 1-Full/post_EECO \
 #5-Ecomorphotype/carnivore 5-Ecomorphotype/omnivore 5-Ecomorphotype/insectivore \
 #1-Full 1-Full/Eocene_only 1-Full/Oligocene_only \
 #4-Tropical_Extratropical/Tropical 4-Tropical_Extratropical/Tropical/Eocene_only 4-Tropical_Extratropical/Tropical/Oligocene_only \
-#4-Tropical_Extratropical/Extratropical 4-Tropical_Extratropical/Extratropical/Eocene_only 4-Tropical_Extratropical/Extratropical/Oligocene_only
+#4-Tropical_Extratropical/Extratropical 4-Tropical_Extratropical/Extratropical/Eocene_only 4-Tropical_Extratropical/Extratropical/Oligocene_only \
+#4-Tropical_Extratropical/Extratropical/post_EECO 4-Tropical_Extratropical/Tropical/post_EECO \
+#5-Ecomorphotype/herbivore  5-Ecomorphotype/herbivore/post_EECO  5-Ecomorphotype/herbivore/Oligocene
 do
 	python ~/Documents/GitHub/CorsaiR/Python/assess_run_convergence.py -dir "$PWD"/../../results/MBD/$trt/ -ana "MBD"
 	Rscript ~/Documents/GitHub/CorsaiR/R/plot_ess.r "$PWD"/../../results/MBD/$trt/ESS_summary.txt "$PWD"/../../results/MBD/$trt/ESS_plot.png
