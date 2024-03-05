45f2b8812a14d5af901f16e6f673a128edf613ec
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-03-05 19:05:55 +0100

    update

--- R/fancy_plots/8-nb_occurrences_and_q_rates.R
+++ R/fancy_plots/8-nb_occurrences_and_q_rates.R
@@ -21,7 +21,22 @@ gsc3$min_age <- sapply(gsc3$min_age, as.numeric)
 gsc3$max_age <- sapply(gsc3$max_age, as.numeric)
 
 ## Function to assess number of occurrences through time -----------------------
-occ_through_time <- function(data){
+occ_through_time <- function(data, what="occ"){ 
+  #what="occ" => occurrence count
+  #what="div_gen" => genus count
+  #what="div_spec" => species count
+  if(what == "div_gen"){
+    data <- data %>%
+      group_by(genus, min_ma, max_ma) %>%
+      distinct(genus) %>% 
+      ungroup()
+  }
+  else if(what == "div_spec"){
+    data <- data %>%
+      group_by(accepted_name, min_ma, max_ma) %>%
+      distinct(accepted_name) %>% 
+      ungroup()
+  }
   max_ma_count <- data %>% count(max_ma)
   min_ma_count <- data %>% count(min_ma)
   
@@ -57,19 +72,19 @@ cumul_occ <- data.frame(time = c(L[[1]][1], L[[1]]),
 
 ## SALMA KEPT ------------------------------------------------------------------
 occ_kept <- readRDS("./data_2023/SPECIES_LISTS/7-Fully_cleaned_EOT_extended_SA_Mammals_SALMA_kept_Tropics_Diet.RDS")
-L <- occ_through_time(occ_kept)
+L_kept <- occ_through_time(occ_kept)
 cumul_occ <- rbind.data.frame(cumul_occ,
-                              data.frame(time = c(L[[1]][1], L[[1]]),
-                                         nb_occ = L[[2]],
+                              data.frame(time = c(L_kept[[1]][1], L_kept[[1]]),
+                                         nb_occ = L_kept[[2]],
                                          salma = "kept"))
 
 ## SMOOTHED --------------------------------------------------------------------
 occ_smoothed <- readRDS("./data_2023/SPECIES_LISTS/8-Fully_cleaned_EOT_extended_SA_Mammals_SALMA_smoothed_Tropics_Diet.RDS")
-L <- occ_through_time(data = occ_smoothed)
+L_smoothed <- occ_through_time(data = occ_smoothed)
 
 cumul_occ <- rbind.data.frame(cumul_occ,
-                              data.frame(time = c(L[[1]][1], L[[1]], 23.2),
-                                         nb_occ = c(L[[2]], 610), #for plotting
+                              data.frame(time = c(L_smoothed[[1]][1], L_smoothed[[1]], 23.2),
+                                         nb_occ = c(L_smoothed[[2]], 610), #for plotting
                                          salma = "smoothed"))
 ## PLOT ------------------------------------------------------------------------
 cumul_occ$salma <- factor(cumul_occ$salma, levels = c("raw", "kept", "smoothed"))
@@ -119,9 +134,12 @@ ggsave("./figures/supp_figs/Occurrence_q_rates/total_cumulative_occurrences.png"
        units = "mm",
        dpi = 400)
   
-  # restricted to the EOT
+  # restricted to the EOT and without raw
+strip_labs <- c("SALMA kept", "SALMA smoothed")
+names(strip_labs) <- c("kept", "smoothed")
+
 cumul_plot_restricted <- cumul_occ %>% 
-  filter(time >= 23.03 & time <= 56) %>%
+  filter(time >= 23.03 & time <= 56 & salma != "raw") %>%
   ggplot(aes(x = time, y = nb_occ, colour = salma)) +
   scale_x_reverse() +
   scale_y_continuous(limits = c(0, 650)) +
@@ -149,6 +167,60 @@ cumul_plot_restricted <- cumul_occ %>%
         panel.background = element_blank()) +
   facet_wrap(.~salma,
              labeller = labeller(salma = strip_labs))
+  
+  # occ number relative to generic diversity
+L_div_kept <- occ_through_time(occ_kept, what = "div_gen")
+L_div_smoothed <- occ_through_time(occ_smoothed, what = "div_gen")
+
+Div_tot <- c(L_div_kept[[2]], L_div_smoothed[[2]])
+Occ_tot <- c(L_kept[[2]], L_smoothed[[2]])
+
+Norm_tot <- sapply(X = 1:length(Div_tot), 
+                   FUN = function(x){
+                     occ <- Occ_tot[x]
+                     div <- Div_tot[x]
+                     if(div == 0){
+                       return(0)
+                     }
+                     else{
+                       return(occ / div)
+                     }
+                   })
+cumul_occ_norm <- data.frame(time = c(L_kept[[1]][1], L_kept[[1]], L_smoothed[[1]][1], L_smoothed[[1]]),
+                             norm_occ = Norm_tot,
+                             salma = c(rep("kept", length(Div_tot)), rep("smoothed", length(Div_tot))))
+    #proper plot
+cumul_diversity_restricted <- cumul_occ_norm %>% 
+  filter(time >= 23.03 & time <= 56) %>%
+  ggplot(aes(x = time, y = norm_occ, colour = salma)) +
+  scale_x_reverse() +
+#  scale_y_continuous(limits = c(0, 650)) +
+  geom_line(linewidth = 2) +
+  scale_colour_manual(values = c("#d95f0e", "#54278f")) +
+  labs(x = NULL,
+       y = "Cumulative normalised nb. of occurrences") +
+  theme(axis.title.x = element_text(size = 20),
+        axis.title.y = element_text(size = 20),
+        axis.text = element_text(size = 15),
+        strip.text = element_text(size = 20),
+        legend.position = "None",
+        panel.background = element_blank()) +
+  coord_geo(pos = list("bottom", "bottom", "bottom"),
+            dat = list(gsc3, gsc2, gsc1),
+            abbrv = list(TRUE, TRUE, FALSE),
+            height = unit(1.5, "line"),
+            center_end_labels = TRUE,
+            size = "auto") +
+  theme(axis.title.x = element_text(size = 20),
+        axis.title.y = element_text(size = 20),
+        axis.text = element_text(size = 15),
+        strip.text = element_text(size = 20),
+        legend.position = "None",
+        panel.background = element_blank()) +
+  facet_wrap(.~salma,
+             labeller = labeller(salma = strip_labs))
+
+cumul_diversity_restricted
 
 ## Q_rates ---------------------------------------------------------------------
 kept_q <- read.table("./results/SALMA_kept/genus_level/1-Full/Q_SHIFTS/Parsed_Q_rates.csv", header = TRUE)
@@ -192,6 +264,11 @@ q_plots <- Q_data %>%
              labeller = labeller(salma = strip_labs))
 
 ## arrange and save
+
+par(mfrow = c(1,2))
+plot(x = cumul_occ_norm$time[which(cumul_occ_norm$salma == "kept")], cumul_occ_norm$norm_occ[which(cumul_occ_norm$salma == "kept")], type = "l")
+plot(x = cumul_occ_norm$time[which(cumul_occ_norm$salma == "smoothed")], cumul_occ_norm$norm_occ[which(cumul_occ_norm$salma == "smoothed")], type = "l")
+
 
 p <- ggarrange(cumul_plot_restricted, q_plots, ncol = 1, labels = c("(A)", "(B)"), font.label = list(size = 18))
 ggsave("./figures/supp_figs/Occurrence_q_rates/cumulative_occ_and_preservation.pdf",
