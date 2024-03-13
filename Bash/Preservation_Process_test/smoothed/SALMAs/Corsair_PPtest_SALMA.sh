0e631ba44906849dff8d77632caf63248d8e6273
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-03-13 18:19:45 +0100

    pptest attempt

---
+++ Bash/Preservation_Process_test/smoothed/SALMAs/Corsair_PPtest_SALMA.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cntr=0
+declare -i cntr
+while [ ! -f result_boxplot.pdf ]; do
+		cntr+=1
+		echo "$cntr round of optimization"
+		echo "python ~/PyRate/PyRate.py $1 -j \$1 -qShift ../../../../data_2023/SALMAs_bins.txt -mG -PPmodeltest > File\$1.log" > tmp_script.sh
+		parallel -j 10 bash tmp_script.sh ::: {1..10}
+		bash ~/Documents/GitHub/CorsaiR/PP_automatest/parse_PPmodeltest_output.sh ${cntr}
+		Rscript ~/Documents/GitHub/CorsaiR/PP_automatest/model_drafting.r tab.tsv ../../../../data_2023/SALMAs_bins.txt
+		rm tab.tsv
+		rm tmp_script.sh
+		sleep 1
+done
+
