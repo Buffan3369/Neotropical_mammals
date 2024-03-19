7a488c61e4f879d9bd00a6eeac37ec5d5deb723d
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-03-19 17:49:45 +0100

    add time bins used in BDCS

--- Bash/BDCS/1-rtt_plot_BDCS.sh
+++ Bash/BDCS/1-rtt_plot_BDCS.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-for dir in SALMA_kept/genus_level/1-Full/BDS_5M SALMA_kept/genus_level/1-Full/BDS SALMA_smoothed/genus_level/1-Full/BDS SALMA_smoothed/genus_level/1-Full/BDS_5M
+#for dir in SALMA_kept/genus_level/1-Full/BDS_5M SALMA_kept/genus_level/1-Full/BDS SALMA_smoothed/genus_level/1-Full/BDS SALMA_smoothed/genus_level/1-Full/BDS_5M
+#do
+#	#Combine logs
+#	python ~/PyRate/PyRate.py -combLog ../../results_EXTENDED/$dir/pyrate_mcmc_logs -tag mcmc -b 10
+#	python ~/PyRate/PyRate.py -combLog ../../results_EXTENDED/$dir/pyrate_mcmc_logs -tag marginal_rates -b 10
+#	mkdir -p ../../results_EXTENDED/$dir/combined_logs
+#	mv ../../results_EXTENDED/$dir/pyrate_mcmc_logs/combined_* ../../results_EXTENDED/$dir/combined_logs
+#	#Plot RTT
+#	python ~/PyRate/PyRate.py -plot2 ../../results_EXTENDED/$dir/combined_logs -b 10
+#done
+
+
+for s in SALMA_kept SALMA_smoothed
 do
-	#Combine logs
-	python ~/PyRate/PyRate.py -combLog ../../results_EXTENDED/$dir/pyrate_mcmc_logs -tag mcmc -b 10
-	python ~/PyRate/PyRate.py -combLog ../../results_EXTENDED/$dir/pyrate_mcmc_logs -tag marginal_rates -b 10
-	mkdir -p ../../results_EXTENDED/$dir/combined_logs
-	mv ../../results_EXTENDED/$dir/pyrate_mcmc_logs/combined_* ../../results_EXTENDED/$dir/combined_logs
-	#Plot RTT
-	python ~/PyRate/PyRate.py -plot2 ../../results_EXTENDED/$dir/combined_logs -b 10
+	for ss in BDCS BDCS_5M
+	do
+		for ana in Tropical Extratropical
+		do
+			dir=$s/genus_level/4-Tropical_Extratropical/$ss/$ana
+			#Combine logs
+			python ~/PyRate/PyRate.py -combLog ../../results_EXTENDED/$dir/pyrate_mcmc_logs -tag mcmc -b 10
+			python ~/PyRate/PyRate.py -combLog ../../results_EXTENDED/$dir/pyrate_mcmc_logs -tag marginal_rates -b 10
+			mkdir -p ../../results_EXTENDED/$dir/combined_logs
+			mv ../../results_EXTENDED/$dir/pyrate_mcmc_logs/combined_* ../../results_EXTENDED/$dir/combined_logs
+			#Plot RTT
+			python ~/PyRate/PyRate.py -plot2 ../../results_EXTENDED/$dir/combined_logs -b 10
+		done
+	done
 done
