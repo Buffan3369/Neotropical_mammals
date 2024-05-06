0210172d4a0a1524c1cbdd81209c468b8bb4574a
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-05-06 12:20:22 +0200

    renaming

--- R/Visualisation/Supp_Info/6bis_tropical_RTT_LTT.R
+++ R/Visualisation/Supp_Info/6bis_tropical_RTT_LTT.R
@@ -1,9 +1,8 @@
 ################################################################################
-# Name: 3-RTT_LTT_BDCS_smoothed.R
+# Name: 6bis_tropical_RTT_LTT.R
 # Author: Lucas Buffan
 # Contact: lucas.l.buffan@gmail.com
-# Aim: Script for figure 1 with diversification rates estimated with BDCS, 
-#       SALMA smoothed.
+# Aim: Same as Figure 3 but with RJMCMC.
 ################################################################################
 
 ## Source helper functions from CorsaiR and additional facilities --------------
@@ -13,28 +12,27 @@ library("readxl")
 library("ggpubr")
 
 ## Figure 1 --------------------------------------------------------------------
-# path to rtt files
-rtt_paths <- c("./results_EXTENDED/SALMA_smoothed/genus_level/1-Full/BDS/",
-               "./results_EXTENDED/SALMA_smoothed/species_level/1-Full/BDCS/",
-               "./results_EXTENDED/SALMA_smoothed/genus_level/4-Tropical_Extratropical/BDCS/Extratropical/",
-               "./results_EXTENDED/SALMA_smoothed/genus_level/4-Tropical_Extratropical/BDCS/Tropical/")
-# path to ltt files
-ltt_path <- c("./results_EXTENDED/SALMA_smoothed/genus_level/1-Full/MH_sampler/",
-              "./results_EXTENDED/SALMA_smoothed/species_level/1-Full/MH_sampler/",
-              "./results_EXTENDED/SALMA_smoothed/genus_level/4-Tropical_Extratropical/RJMCMC/Extratropical/",
-              "./results_EXTENDED/SALMA_smoothed/genus_level/4-Tropical_Extratropical/RJMCMC/Tropical/")
+# path to diversification rates and diversity files
+paths <- c("./results_EXTENDED/SALMA_smoothed/genus_level/4-Tropical_Extratropical/RJMCMC/Extratropical/",
+           "./results_EXTENDED/SALMA_smoothed/genus_level/4-Tropical_Extratropical/RJMCMC/Tropical/")
+# first GTS
+gsc1 <- deeptime::epochs
 # second GTS
 gsc2 <- read_xlsx("./data_2023/time_bins/EarlyMidLate_epochs.xlsx")
 gsc2 <- gsc2 %>% rename(min_age = "min_ma", max_age = "max_ma", name = "interval_name")
-# deviation for BDS rates
-delta <- 0.3
+# weird GTS modification for plotting
+gsc1_bis <- gsc1[5:6,]
+gsc1_bis$max_age[nrow(gsc1_bis)] <- 52
+gsc1_bis$min_age[1] <- 24
+gsc2_bis <- gsc2[12:16,]
+gsc2_bis$max_age[nrow(gsc2_bis)] <- 52
+gsc2_bis$min_age[1] <- 24
 # plot list
 plot_list1 <- list()
 i <- 0
 j <- 0
-for(k in 1:4){
-  rtt_file <- list.files(paste0(rtt_paths[k], "combined_logs/"), pattern = "_RTT.r") #character of length 1
-  rtt_tbl <- extract_rtt(path = paste0(rtt_paths[k], "combined_logs/", rtt_file), ana = "BDS")
+for(k in 1:2){
+  rtt_tbl <- extract_rtt(path = paste0(paths[k], "combined_logs/RTT_plots.r"), ana = "RJMCMC")
   # in case doesn't go far enough
   if(max(rtt_tbl$time) < 52){
     rtt_tbl$time[nrow(rtt_tbl)] <- 52
@@ -49,28 +47,14 @@ for(k in 1:4){
   x.axis <- FALSE
   xlim <- c(52, 24)
   display_gts <- FALSE
-  # adjust age of shifts
-  s <- 0 # just for extra-tropical
-  if(k == 3){
-    s <- 1
-  }
-  rtt_tbl$time[which(rtt_tbl$time == 49-s)] <- 47.8 + delta
-  rtt_tbl$time[which(rtt_tbl$time == 48-s)] <- 47.8 - delta
-  rtt_tbl$time[which(rtt_tbl$time == 39-s)] <- 37.71 + delta
-  rtt_tbl$time[which(rtt_tbl$time == 38-s)] <- 37.71 - delta
-  rtt_tbl$time[which(rtt_tbl$time == 35-s)] <- 33.9 + delta
-  rtt_tbl$time[which(rtt_tbl$time == 34-s)] <- 33.9 - delta
-  rtt_tbl$time[which(rtt_tbl$time == 29-s)] <- 27.8 + delta
-  rtt_tbl$time[which(rtt_tbl$time == 28-s)] <- 27.8 - delta
   # gts stuff
   i <- i+1
-  if(i >= 10){
+  if(i >= 4){
     x_lab <- "Time (Ma)"
     x.axis <- TRUE
     xlim <- NULL
     display_gts <- TRUE
   }
-  
   # diversification rates plot ---------
   rtt_plt <- rtt_plot(data = rtt_tbl,
                       type = "SpEx",
@@ -91,7 +75,8 @@ for(k in 1:4){
                       x.axis = x.axis,
                       display_EECO_MECO = TRUE,
                       several_gts = TRUE,
-                      geoscale2 = gsc2,
+                      geoscale = gsc1_bis,
+                      geoscale2 = gsc2_bis,
                       geoscale_height = unit(1, "line"),
                       abbr = list(TRUE, FALSE)) +
     # Temporal bands
@@ -117,7 +102,7 @@ for(k in 1:4){
   plot_list1[[i+j]] <- NULL
   # net rate plot ---------------------
   i <- i+1
-  if(i >= 10){
+  if(i >= 4){
     x_lab <- "Time (Ma)"
     x.axis <- TRUE
     xlim <- NULL
@@ -126,9 +111,9 @@ for(k in 1:4){
   net_plt <- rtt_plot(data = rtt_tbl,
                       type = "net",
                       x_lab = x_lab,
-                      y_breaks = seq(-0.5, 0.5, 0.1),
-                      y_limits = c(-0.55, 0.55),
-                      restrict_thr = 0.55,
+                      y_breaks = seq(-0.2, 0.4, 0.1),
+                      y_limits = c(-0.45, 0.45),
+                      restrict_thr = 0.45,
                       stage_x_breaks = FALSE,
                       manual_x_breaks = seq(25, 50, 5),
                       axes.labelsize=15,
@@ -140,7 +125,8 @@ for(k in 1:4){
                       x.axis = x.axis,
                       display_EECO_MECO = TRUE,
                       several_gts = TRUE,
-                      geoscale2 = gsc2,
+                      geoscale = gsc1_bis,
+                      geoscale2 = gsc2_bis,
                       geoscale_height = unit(1, "line"),
                       abbr = list(TRUE, FALSE)) +
     # Temporal bands
@@ -151,11 +137,11 @@ for(k in 1:4){
   #add labels in the first line  
   if(i == 2){
     net_plt <- net_plt +
-      annotate(geom = "segment", x = 47, xend = 50, y = 0.4, yend = 0.4, colour = "#504A4B", linewidth = 1) +
-      annotate(geom = "text", x = 40.5, y = 0.4, label = "Net diversification\n rate", size = 5) +
-      annotate(geom = "text", x = 31.5, y = 0.5, label = "EOT", size = 7, colour = "red") +
-      geom_text(aes(x = 51.5, y = -0.45, label = "EECO"), angle = 90, colour = "bisque4") +
-      geom_text(aes(x = 41.25, y = -0.45, label = "MECO"), angle = 90, colour = "bisque4")
+      annotate(geom = "segment", x = 47, xend = 50, y = 0.35, yend = 0.35, colour = "#504A4B", linewidth = 1) +
+      annotate(geom = "text", x = 40.5, y = 0.35, label = "Net diversification\n rate", size = 5) +
+      annotate(geom = "text", x = 31.5, y = 0.4, label = "EOT", size = 7, colour = "red") +
+      geom_text(aes(x = 51.5, y = -0.325, label = "EECO"), angle = 90, colour = "bisque4") +
+      geom_text(aes(x = 41.25, y = -0.325, label = "MECO"), angle = 90, colour = "bisque4")
   }
   plot_list1[[i+j]] <- net_plt
   #add blank element to plot list
@@ -163,14 +149,18 @@ for(k in 1:4){
   plot_list1[[i+j]] <- NULL
   # LTT plot --------------------------
   i <- i+1
-  if(i >= 10){
+  lgth_break <- 20
+  if(k == 2){
+    lgth_break <- 10
+  }
+  if(i >= 4){
     x_lab <- "Time (Ma)"
     x.axis <- TRUE
     xlim <- NULL
     display_gts <- TRUE
   }
-  ltt <- list.files(paste0(ltt_path[k], "LTT/"), pattern = "se_est_ltt.txt")
-  ltt_tbl <- read.table(paste0(ltt_path[k], "LTT/", ltt), header = TRUE)
+  ltt <- list.files(paste0(paths[k], "LTT/"), pattern = "se_est_ltt.txt")[[1]]
+  ltt_tbl <- read.table(paste0(paths[k], "LTT/", ltt), header = TRUE)
   ltt_tbl <- ltt_tbl %>%
     rename("Age" = time, "Diversity" = diversity, "min_Diversity" = m_div, "max_Diversity" = M_div) %>%
     filter(Age > 24 & Age < 52)
@@ -184,7 +174,7 @@ for(k in 1:4){
                        axes.labelsize=15,
                        ticks.labelsize = 12,
                        x_lab = x_lab,
-                       y_breaks = seq(0,(round(max(ltt_tbl$Diversity), -1) + 10),20), 
+                       y_breaks = seq(0,(round(max(ltt_tbl$Diversity), -1) + 10), lgth_break), 
                        y_limits = c(0,(round(max(ltt_tbl$Diversity), -1) + 22)),
                        display_gts = display_gts,
                        xlim = xlim,
@@ -194,7 +184,8 @@ for(k in 1:4){
                        x.axis = x.axis,
                        display_EECO_MECO = TRUE,
                        several_gts = TRUE,
-                       geoscale2 = gsc2,
+                       geoscale = gsc1_bis,
+                       geoscale2 = gsc2_bis,
                        geoscale_height = unit(1, "line"),
                        geoscale_labelsize = 4,
                        abbr = list(TRUE, FALSE)) +
@@ -209,32 +200,23 @@ for(k in 1:4){
       annotate(geom = "segment", x = 42.5, xend = 45, y = 110, yend = 110, colour = "#006d2c", linewidth = 1) +
       annotate(geom = "text", x = 39.25, y = 110, label = "Diversity", size = 5) +
       annotate(geom = "text", x = 31.5, y = 115, label = "EOT", size = 7, colour = "red") +
-      geom_text(aes(x = 51.5, y = 11, label = "EECO"), angle = 90, colour = "bisque4") +
-      geom_text(aes(x = 41.25, y = 11, label = "MECO"), angle = 90, colour = "bisque4")
+      geom_text(aes(x = 51.5, y = 18, label = "EECO"), angle = 90, colour = "bisque4") +
+      geom_text(aes(x = 41.25, y = 18, label = "MECO"), angle = 90, colour = "bisque4")
     
   }
   plot_list1[[i+j]] <- ltt.plot
 }
 
-fig1 <- ggarrange(plotlist = plot_list1, nrow = 4, ncol = 5, 
-                  heights = c(1, 1, 1, 1.25),
+fig3 <- ggarrange(plotlist = plot_list1, nrow = 2, ncol = 5, 
+                  heights = c(1, 1.25),
                   widths = c(1, 0.05, 1, 0.05, 1),
                   labels = c("(A)", NA, "(B)", NA, "(C)",
-                             "(D)",  NA, "(E)", NA, "(F)",
-                             "(G)",  NA, "(H)",  NA, "(I)",
-                             "(J)",  NA, "(K)",  NA, "(L)"),
-                  hjust = -0.4, 
+                             "(D)",  NA, "(E)", NA, "(F)"),
+                  hjust = 0,
                   font.label = list(size = 18))
 
-ggsave("./figures/Figure_1/backbone_bdcs.pdf",
-       plot = fig1,
-       height = 400,
+ggsave("./figures/supp_figs/RTT_LTT_trop_etrop.pdf",
+       plot = fig3,
+       height = 200,
        width = 400,
-       units = "mm")
-
-ggsave("./figures/Figure_1/backbone_bdcs.png",
-       plot = fig1,
-       height = 400,
-       width = 400,
-       dpi = 600,
        units = "mm")
