e003379b262aa1f7572ddd7739157de58d6b9789
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-05-29 14:02:46 +0200

    fix ADE issues

--- Bash/ADE/1-run_ADE.sh
+++ Bash/ADE/1-run_ADE.sh
@@ -2,17 +2,22 @@
 
 ## SALMA smoothed ##
 	# early phase
-echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_smoothed/genus_level/1-Full_early/*_PyRate.py -j \$1 -filter 56 39 -qShift EO_stages.txt -mG -n 50000000 -s 50000" > tmp_script_ade.sh
-parallel -j 20 bash tmp_script_ade.sh ::: {1..20}
+#echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_smoothed/genus_level/1-Full_early/*_PyRate.py -j \$1 -filter 56 39 -qShift EO_stages.txt -mG -n 50000000 -s 50000" > tmp_script_ade.sh
+#parallel -j 20 bash tmp_script_ade.sh ::: {1..20}
 	# late phase
-echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_smoothed/genus_level/1-Full_late/*_PyRate.py -j \$1 -filter 39 23 -qShift EO_stages.txt -mG -n 50000000 -s 50000" > tmp_script_ade.sh
-parallel -j 20 bash tmp_script_ade.sh ::: {1..20}
+#echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_smoothed/genus_level/1-Full_late/*_PyRate.py -j \$1 -filter 39 23 -qShift EO_stages.txt -mG -n 50000000 -s 50000" > tmp_script_ade.sh
+#parallel -j 20 bash tmp_script_ade.sh ::: {1..20}
 
 ## SALMA kept ##
 	# early phase
-echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_kept/genus_level/1-Full_early/*_PyRate.py -j \$1 -filter 56 39 -qShift EO_stages.txt -mG -n 50000000 -s 50000" > tmp_script_ade.sh
-parallel -j 20 bash tmp_script_ade.sh ::: {1..20}
-	# late phase
-echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_kept/genus_level/1-Full_late/*_PyRate.py -j \$1 -filter 39 23 -qShift EO_stages.txt -mG -n 50000000 -s 50000" > tmp_script_ade.sh
+#echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_kept/genus_level/1-Full_early/*_PyRate.py -j \$1 -filter 56 39 -qShift EO_stages.txt -mG -n 50000000 -s 50000" > tmp_script_ade.sh
+#parallel -j 20 bash tmp_script_ade.sh ::: {1..20}
+#	# late phase
+#echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_kept/genus_level/1-Full_late/*_PyRate.py -j \$1 -filter 39 23 -qShift EO_stages.txt -mG -n 50000000 -s 50000" > tmp_script_ade.sh
 
+#parallel -j 20 bash tmp_script_ade.sh ::: {1..20}
+
+
+	# SALMA smoothed late phase without -mG
+echo "python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_smoothed/genus_level/1-Full_late/*_PyRate.py -j \$1 -filter 39 23 -qShift ../../data_2023/Extended_EO_stages.txt -n 50000000 -s 50000" > tmp_script_ade.sh
 parallel -j 20 bash tmp_script_ade.sh ::: {1..20}
--- Bash/ADE/tmp_script_ade.sh
+++ Bash/ADE/tmp_script_ade.sh
@@ -1,1 +1,1 @@
-python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_smoothed/genus_level/1-Full_early/*_PyRate.py -j $1 -filter 56 39 -qShift EO_stages.txt -n 50000000 -s 50000
+python3 ~/PyRate/PyRate.py -ADE 1 ../../results_EXTENDED/ADE/SALMA_smoothed/genus_level/1-Full_late/*_PyRate.py -j $1 -filter 39 23 -qShift ../../data_2023/Extended_EO_stages.txt -n 50000000 -s 50000
