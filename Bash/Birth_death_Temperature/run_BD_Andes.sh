f22e679c4d73eb604f01521a0a06bacb6f84043b
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-07-31 17:35:32 +0200

    BDT updates

--- Bash/Birth_death_Temperature/run_BD_Andes.sh
+++ Bash/Birth_death_Temperature/run_BD_Andes.sh
@@ -2,10 +2,10 @@
 
 ## Birth Death model with time-continuous correlate applied to andean uplift 
         # Post EECO
-echo "python3 ~/PyRate/PyRateContinuous.py -d ../../BDT/Corr_andes/post_EECO/*se_est.txt -c ../../BDT/andes_EECO/2-Andes_mean_elevations_post_EECO_500ky_step.txt -j \$1 -n 10000000 -s 10000" > tmp_script_andes.sh
+echo "python3 ~/PyRate/PyRateContinuous.py -d ../../BDT/Corr_andes/post_EECO/*se_est.txt -c ../../BDT/predictors/2-Andes_mean_elevations_post_EECO_500ky_step.txt -j \$1 -n 10000000 -s 10000" > tmp_script_andes.sh
 /usr/local/bin/parallel -j 10 bash tmp_script_andes.sh ::: {1..10}
 
         # Oligocene only
-echo "python3 ~/PyRate/PyRateContinuous.py -d ../../BDT/Corr_andes/Oligocene_only/*se_est.txt -c ../../BDT/andes_Oligocene/2-Andes_mean_elevations_Oligocene_only_500ky_step.txt -j \$1 -n 10000000 -s 10000" > tmp_script_andes.sh
+echo "python3 ~/PyRate/PyRateContinuous.py -d ../../BDT/Corr_andes/Oligocene_only/*se_est.txt -c ../../BDT/predictors/2-Andes_mean_elevations_Oligocene_only_500ky_step.txt -j \$1 -n 10000000 -s 10000" > tmp_script_andes.sh
 /usr/local/bin/parallel -j 10 bash tmp_script_andes.sh ::: {1..10}
 
