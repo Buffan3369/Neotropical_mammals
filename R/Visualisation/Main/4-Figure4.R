57d30a7f4ad85b0d35f80610d4bb161a3a7423aa
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-10-28 11:55:47 +0100

    invert Eocene and Oligocene rows in Fig 4

--- R/Visualisation/Main/4-Figure4.R
+++ R/Visualisation/Main/4-Figure4.R
@@ -12,12 +12,12 @@ source("./R/useful/4b-MBD_accessory.R")
 
 ## Define covariable names -----------------------------------------------------
 covar_idx <- hash("0" = "Self-diversity",
-                "1" = "Plant Diversity",
-                "2" = "Andes Elevation",
-                "3" = "Global Temperature",
-                "4" = "Sea Level",
-                "5" = "Habitat Openness",
-                "6" = "Forest Density")
+                  "1" = "Plant Diversity",
+                  "2" = "Andes Elevation",
+                  "3" = "Global Temperature",
+                  "4" = "Sea Level",
+                  "5" = "Habitat Openness",
+                  "6" = "Forest Density")
 
 ## Set strip labels for plots --------------------------------------------------
 rate.labs <- c("Extinction rate", "Origination rate")
@@ -100,13 +100,16 @@ for(salma in c("SALMA_smoothed")){
       }
       PLOT_DF <- PLOT_DF[-c(1),]
       SIGNIF_DF <- SIGNIF_DF[-c(1),]
+      #change Eocene Oligocene order
+      PLOT_DF$interval <- factor(PLOT_DF$interval, levels = c("Oligocene", "Eocene"))
+      SIGNIF_DF$interval <- factor(SIGNIF_DF$interval, levels = c("Oligocene", "Eocene"))
       #plot
       MBD_viol <- MBD.plot(PLOT_DF = PLOT_DF,
                            SIGNIF_DF = SIGNIF_DF,
                            x_breaks = 0:6,
                            x_labels = c(values(covar_idx)),
                            rate.labs = rate.labs,
-                           int.labs = c("Eocene", "Oligocene"),
+                           int.labs = c("Oligocene", "Eocene"),
                            vjust.star.ori = 0.4,
                            vjust.star.ext = 0.4)
       ggsave(paste0("./figures/Main/Figure_4/Full_", salma, ".pdf"),
@@ -118,36 +121,39 @@ for(salma in c("SALMA_smoothed")){
     # loop across orders
     if(strsplit(dir, split = "-")[[1]][1] == "6"){
       for(odr in c("Notoungulata")){
-      # initialise large tables
-      PLOT_DF1 <- data.frame(param = NA, rate = NA, col = NA, signif_col = NA, value = NA, interval = NA)
-      SIGNIF_DF1 <- data.frame(param = NA, rate = NA, col = NA, max_val = NA, min_val = NA, star_pos = NA, interval = NA)
-      for(trt in c("post_EECO/", "Oligocene_only/")){
-        int <- ifelse(trt == "post_EECO/", "Eocene", "Oligocene")
-        rid <- paste0("./results_EXTENDED/MBD/", salma, "/genus_level/", dir, odr, "/", trt)
-        tmp <- out_table_MBD(rid, interval = int)
-        plot_df1 <-tmp[[1]]
-        signif_df1 <- tmp[[2]]
-        # Extend the big berthas
-        PLOT_DF1 <- rbind.data.frame(PLOT_DF1, plot_df1)
-        SIGNIF_DF1 <- rbind.data.frame(SIGNIF_DF1, signif_df1)
-      }
-      # remove initialising rows
-      PLOT_DF1 <- PLOT_DF1[-c(1),]
-      SIGNIF_DF1 <- SIGNIF_DF1[-c(1),]
-      # plot
-      MBD_viol1 <- MBD.plot(PLOT_DF = PLOT_DF1,
-                            SIGNIF_DF = SIGNIF_DF1,
-                            x_breaks = 0:6,
-                            x_labels = c(values(covar_idx)),
-                            rate.labs = rate.labs,
-                            vjust.star.ori = 0.4,
-                            vjust.star.ext = 0.4,
-                            int.labs = c("Eocene", "Oligocene"))
-      ggsave(paste0("./figures/Main/Figure_4/", odr,"_", salma, ".pdf"),
-             plot = MBD_viol1,
-             height = 300,
-             width = 400,
-             units = "mm")
+        # initialise large tables
+        PLOT_DF1 <- data.frame(param = NA, rate = NA, col = NA, signif_col = NA, value = NA, interval = NA)
+        SIGNIF_DF1 <- data.frame(param = NA, rate = NA, col = NA, max_val = NA, min_val = NA, star_pos = NA, interval = NA)
+        for(trt in c("post_EECO/", "Oligocene_only/")){
+          int <- ifelse(trt == "post_EECO/", "Eocene", "Oligocene")
+          rid <- paste0("./results_EXTENDED/MBD/", salma, "/genus_level/", dir, odr, "/", trt)
+          tmp <- out_table_MBD(rid, interval = int)
+          plot_df1 <-tmp[[1]]
+          signif_df1 <- tmp[[2]]
+          # Extend the big berthas
+          PLOT_DF1 <- rbind.data.frame(PLOT_DF1, plot_df1)
+          SIGNIF_DF1 <- rbind.data.frame(SIGNIF_DF1, signif_df1)
+        }
+        # remove initialising rows
+        PLOT_DF1 <- PLOT_DF1[-c(1),]
+        SIGNIF_DF1 <- SIGNIF_DF1[-c(1),]
+        #change Eocene Oligocene order
+        PLOT_DF1$interval <- factor(PLOT_DF1$interval, levels = c("Oligocene", "Eocene"))
+        SIGNIF_DF1$interval <- factor(SIGNIF_DF1$interval, levels = c("Oligocene", "Eocene"))
+        # plot
+        MBD_viol1 <- MBD.plot(PLOT_DF = PLOT_DF1,
+                              SIGNIF_DF = SIGNIF_DF1,
+                              x_breaks = 0:6,
+                              x_labels = c(values(covar_idx)),
+                              rate.labs = rate.labs,
+                              vjust.star.ori = 0.4,
+                              vjust.star.ext = 0.4,
+                              int.labs = c("Oligocene", "Eocene"))
+        ggsave(paste0("./figures/Main/Figure_4/", odr,"_", salma, ".pdf"),
+               plot = MBD_viol1,
+               height = 300,
+               width = 400,
+               units = "mm")
       }
     }
   }
