cf858de923c2f0ae5621c846ffed1f3010eb8e6f
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2024-11-09 10:12:43 +0100

    implement conservative and stringent partionings

--- R/Data_processing/6-Split_TsTe_interval.R
+++ R/Data_processing/6-Split_TsTe_interval.R
@@ -15,32 +15,53 @@ for(salma in c("SALMA_smoothed", "SALMA_kept")){
   tste_file <- list.files(path = tste_dir, pattern = "se_est.txt") # single file anyway
   tste_full <- read.table(paste(tste_dir, tste_file, sep = "/"),
                           header = TRUE)
-  # Assessing bin midpoints
+  # Assessing mean ts/te per taxa
   mean_ts <- apply(X = tste_full[, seq(3, ncol(tste_full)-1), 2],
                    MARGIN = 1,
                    FUN = mean)
   mean_te <- apply(X = tste_full[, seq(4, ncol(tste_full), 2)],
                    MARGIN = 1,
                    FUN = mean)
+  # Assessing bin midpoints
   mean_tste_df <- data.frame(ts = mean_ts, te = mean_te)
   mean_tste_df <- mean_tste_df %>% 
     mutate(mid = (ts+te)/2)
+  
+#### CONSERVATIVE SPLIT ####
   # Eoc_Olig
   subset_idx_eot <- which(mean_tste_df$mid <= 56 & mean_tste_df$mid >= 23.03)
   full_eot <- tste_full[subset_idx_eot, ]
   write.table.lucas(x = full_eot,
-                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full/Full_Eocene_Oligocene_", salma, "_se_est.txt"))
+                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full_conservative_partitioning/Full_Eocene_Oligocene_", salma, "_se_est.txt"))
   # Post EECO
   subset_idx_EECO <- which(mean_tste_df$mid <= 52 & mean_tste_df$mid >= 33.9)
   post_EECO <- tste_full[subset_idx_EECO,]
   write.table.lucas(x = post_EECO,
-                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full/post_EECO/Full_post_EECO_", salma, "_se_est.txt"))
+                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full_conservative_partitioning/post_EECO/Full_post_EECO_", salma, "_se_est.txt"))
   # Oligocene only
   subset_idx_olig <- which(mean_tste_df$mid <= 33.9 & mean_tste_df$mid >= 23.03)
   olig <- tste_full[subset_idx_olig,]
-  write.table.lucas(x = tste_full %>% filter(ts <= 34 & te >= 23),
-                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full/Oligocene_only/Full_Oligocene_only_", salma, "_se_est.txt"))
+  write.table.lucas(x = olig,
+                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full_conservative_partitioning/Oligocene_only/Full_Oligocene_only_", salma, "_se_est.txt"))
 
+##### STRINGENT SPLIT #####
+  subset_idx_eot <- which(mean_tste_df$ts <= 56 & mean_tste_df$te >= 23.03)
+  full_eot <- tste_full[subset_idx_eot, ]
+  write.table.lucas(x = full_eot,
+                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full_stringent_partitioning/Full_Eocene_Oligocene_", salma, "_se_est.txt"))
+  # Post EECO
+  subset_idx_EECO <- which(mean_tste_df$ts <= 52 & mean_tste_df$te >= 33.9)
+  post_EECO <- tste_full[subset_idx_EECO,]
+  write.table.lucas(x = post_EECO,
+                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full_stringent_partitioning/post_EECO/Full_post_EECO_", salma, "_se_est.txt"))
+  # Oligocene only
+  subset_idx_olig <- which(mean_tste_df$ts <= 33.9 & mean_tste_df$te >= 23.03)
+  olig <- tste_full[subset_idx_olig,]
+  write.table.lucas(x = olig,
+                    file = paste0("./results_EXTENDED/MBD/", salma, "/genus_level/1-Full_stringent_partitioning/Oligocene_only/Full_Oligocene_only_", salma, "_se_est.txt"))
+  
+  
+    
   # for(odr in keys(tax_dict)){
   #   tste_dir <- paste0("./results_EXTENDED/", salma, "/genus_level/6-Order_level/", odr, "/LTT")
   #   tste_file <- list.files(path = tste_dir, pattern = "se_est.txt") # single file anyway
@@ -91,22 +112,45 @@ for(lat in c("Tropical", "Extratropical")){
   mean_tste_df <- data.frame(ts = mean_ts, te = mean_te)
   mean_tste_df <- mean_tste_df %>% 
     mutate(mid = (ts+te)/2)
+  
+#### CONSERVATIVE SPLIT ####
   # Eoc_Olig
   subset_idx_eot <- which(mean_tste_df$mid <= 56 & mean_tste_df$mid >= 23.03)
   full_eot <- tste_full[subset_idx_eot, ]
   write.table.lucas(x = full_eot,
-                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff/", 
+                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff_conservative/", 
                                   values(corr_lat[lat]), "/", values(corr_lat[lat]), "_Eocene_Oligocene_se_est.txt"))
   # post_EECO
   subset_idx_EECO <- which(mean_tste_df$mid <= 52 & mean_tste_df$mid >= 33.9)
   post_EECO <- tste_full[subset_idx_EECO,]
   write.table.lucas(x = post_EECO,
-                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff/", 
+                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff_conservative/", 
                                   values(corr_lat[lat]), "/post_EECO/", values(corr_lat[lat]), "_post_EECO_se_est.txt"))
   # Oligocene Only
   subset_idx_olig <- which(mean_tste_df$mid <= 33.9 & mean_tste_df$mid >= 23.03)
   olig <- tste_full[subset_idx_olig,]
   write.table.lucas(x = olig,
-                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff/", 
-                                  values(corr_lat[lat]), "/Oligocene_only/", values(corr_lat[lat]), "_Oligocene_only_se_est.txt"))  
+                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff_conservative/", 
+                                  values(corr_lat[lat]), "/Oligocene_only/", values(corr_lat[lat]), "_Oligocene_only_se_est.txt"))
+
+#### STRINGENT SPLIT ####
+  # Eoc_Olig
+  subset_idx_eot <- which(mean_tste_df$ts <= 56 & mean_tste_df$te >= 23.03)
+  full_eot <- tste_full[subset_idx_eot, ]
+  write.table.lucas(x = full_eot,
+                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff_stringent/", 
+                                  values(corr_lat[lat]), "/", values(corr_lat[lat]), "_Eocene_Oligocene_se_est.txt"))
+  # post_EECO
+  subset_idx_EECO <- which(mean_tste_df$ts <= 52 & mean_tste_df$te >= 33.9)
+  post_EECO <- tste_full[subset_idx_EECO,]
+  write.table.lucas(x = post_EECO,
+                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff_stringent/", 
+                                  values(corr_lat[lat]), "/post_EECO/", values(corr_lat[lat]), "_post_EECO_se_est.txt"))
+  # Oligocene Only
+  subset_idx_olig <- which(mean_tste_df$ts <= 33.9 & mean_tste_df$te >= 23.03)
+  olig <- tste_full[subset_idx_olig,]
+  write.table.lucas(x = olig,
+                    file = paste0("./results_EXTENDED/MBD/SALMA_smoothed/genus_level/andes_diff_stringent/", 
+                                  values(corr_lat[lat]), "/Oligocene_only/", values(corr_lat[lat]), "_Oligocene_only_se_est.txt"))
+  
 }
